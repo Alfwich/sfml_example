20bf6875bd2a38acf94b0f425e26aab7a87c55c3
Arthur Wuterich <arthurwut@gmail.com>
2021-11-13 10:03:27 -0800

    Initial Example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use sfml::*;
+use sfml::window::{Window, Event, Style};
 
 fn main() {
-    println!("Hello, world!");
+    let mut window = Window::new((800, 600), "SFML Example", Style::CLOSE, &Default::default());
+    window.set_framerate_limit(60);
+    
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            if event == Event::Closed {
+                window.close();
+            }
+        }
+        
+        window.set_active(true);
+        
+        window.display();
+    }
+    
 }
